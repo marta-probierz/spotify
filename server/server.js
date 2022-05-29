02ed866e87112999adb896891277ef46d8661f22
Marta Probierz <marta.anna.probierz@gmail.com>
2022-05-29 21:37:29 +0200

    add some server staff

---
+++ server/server.js
@@ -0,0 +1,28 @@
+import SpotifyWebApi from 'spotify-web-api-node';
+import express from 'express';
+
+const app = express();
+
+app.post('/login', (req, res) => {
+  const code = req.body.code;
+  const spotifyAPI = new SpotifyWebApi({
+    redirectUri: process.env.REDIRECT_URI,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+  });
+
+  spotifyAPI
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch(() => {
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
