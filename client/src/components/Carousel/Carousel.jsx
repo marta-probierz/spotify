579d194c47efccf0d9941e09e03adb1897579dca
Marta Probierz <marta.anna.probierz@gmail.com>
2022-07-03 09:22:11 +0200

    ref

--- client/src/components/Carousel/Carousel.jsx
+++ client/src/components/Carousel/Carousel.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Slider from 'infinite-react-carousel';
 import { useTranslation } from 'react-i18next';
 
-import './Carousel.scss';
+import styles from './Carousel.module.scss';
 import login from './Media/login.png';
 import play from './Media/play.png';
 import enjoy from './Media/enjoy.png';
@@ -19,19 +19,19 @@ export const Carousel = () => {
     wheel: true,
   };
   return (
-    <div className="container">
+    <div className={styles.container}>
       <Slider {...settings}>
         <div>
-          <img src={login} className="slide_img" alt="login" />
-          <span className="text">{t`carousel.login`}</span>
+          <img src={login} className={styles.slide_img} alt="login" />
+          <span className={styles.text}>{t`carousel.login`}</span>
         </div>
         <div>
-          <img src={play} className="slide_img" alt="play" />
-          <span className="text">{t`carousel.play`}</span>
+          <img src={play} className={styles.slide_img} alt="play" />
+          <span className={styles.text}>{t`carousel.play`}</span>
         </div>
         <div>
-          <img src={enjoy} className="slide_img" alt="enjoy" />
-          <span className="text">{t`carousel.enjoy`}</span>
+          <img src={enjoy} className={styles.slide_img} alt="enjoy" />
+          <span className={styles.text}>{t`carousel.enjoy`}</span>
         </div>
       </Slider>
     </div>
