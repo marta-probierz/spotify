f5d3a2ab985c9309a0648a2fb2e0d74e7d4bb3a6
Marta Probierz <marta.anna.probierz@gmail.com>
2022-07-03 09:14:01 +0200

    add carousel

--- client/src/pages/Login/Login.jsx
+++ client/src/pages/Login/Login.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { useTranslation } from 'react-i18next';
 
 import styles from './Login.module.scss';
-import { Button } from '../../components';
+import { Button, Carousel } from '../../components';
 
 const AUTH_URL =
   'https://accounts.spotify.com/authorize?client_id=cfd8c73925254678879690bde1a4903c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state';
@@ -17,7 +17,9 @@ export const Login = () => {
         <div className={styles.then}>{t`home.then`}</div>
         <Button url={AUTH_URL} label={t`home.loginButton`} />
       </div>
-      <div className={styles.containerRight}></div>
+      <div className={styles.containerRight}>
+        <Carousel />
+      </div>
     </div>
   );
 };
